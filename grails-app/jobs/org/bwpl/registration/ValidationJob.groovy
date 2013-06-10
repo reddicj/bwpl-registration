82923d9fcec8834ca8912c16166f4f3cf5c95a4a
James Reddick <reddicj@gmail.com>
2013-06-11 00:23:08 +0100

    Change cron validation jon to once every Monday at 2am.

--- grails-app/jobs/org/bwpl/registration/ValidationJob.groovy
+++ grails-app/jobs/org/bwpl/registration/ValidationJob.groovy
@@ -6,8 +6,8 @@ import org.bwpl.registration.validation.Validator
 class ValidationJob {
 
     static triggers = {
-        // cron name: 'weeklySundayTrigger', cronExpression: "0 0 2 * * 1"
-        cron name: 'testNightlyTrigger', cronExpression: "0 0 2 * * ?"
+        cron name: 'weeklyMondayTrigger', cronExpression: "0 0 2 * * 2"
+        // cron name: 'testNightlyTrigger', cronExpression: "0 0 2 * * ?"
         // simple name: 'testTrigger', startDelay: 120000, repeatInterval: 120000
     }
 
